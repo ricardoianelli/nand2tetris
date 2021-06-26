4abcd8892eb3f1bb0b9d604a2d97d4e36103c1d9
ricardoianelli <ricardoianelli@hotmail.com>
2021-06-26 18:33:23 -0300

    Add fill machine lang code

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,81 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+//screen_color: (0) white, (1) black
+
+@screen_color
+M=0
+@SCREEN
+D=A
+@8192
+D=D+A
+@max
+M=D
+
+(RESET)
+	@SCREEN
+	D=A
+	@curr_addr
+	M=D
+
+(MAINLOOP)
+	@KBD
+	D=M
+	@PREBLACK
+	D;JGT
+	@PREWHITE
+	0;JMP
+
+(PREBLACK)
+	@screen_color
+	D=M
+	@MAINLOOP
+	D;JGT
+	@BLACK
+	0;JMP
+
+(PREWHITE)
+	@screen_color
+	D=M
+	@MAINLOOP
+	D;JEQ
+	@WHITE
+	0;JMP
+
+(BLACK)
+	@screen_color
+	M=1
+	@curr_addr
+	A=M
+	M=-1
+	@curr_addr
+	M=M+1
+	@max
+	D=M
+	@curr_addr
+	D=D-M
+	@BLACK
+	D;JGT
+	@RESET
+	0;JMP
+
+(WHITE)
+	@screen_color
+	M=0
+	@curr_addr
+	A=M
+	M=0
+	@curr_addr
+	M=M+1
+	@max
+	D=M
+	@curr_addr
+	D=D-M
+	@WHITE
+	D;JGT
+	@RESET
+	0;JMP
+
+(END)
+	@END
+	0;JMP
